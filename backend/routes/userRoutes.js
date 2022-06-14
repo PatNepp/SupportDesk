7335be34c654c46a55e81f96f1c803e129bbaca6
Patrick Nepp <ptnepp@gmail.com>
2022-06-14 14:32:11 -0500

    protectd routes and token auth

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const {registerUser, loginUser} = require('../controllers/userController')
+const {registerUser, loginUser, getMe} = require('../controllers/userController')
+// to use protect just put it in the parenthesis as second argument
+const {protect} = require('../middleware/authMiddleware')
 
 //When using a controller, you can plug in functions instead of typing them into the route
 router.post('/', registerUser)
@@ -12,5 +14,6 @@ router.post('/login', loginUser)
 // router.post('/login', (req, res) => {
 //     res.send('login route')
 // })
+router.get('/me',protect, getMe)
 
 module.exports = router
